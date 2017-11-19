75cc686a41fd593d6d2602c71d9a457465b2e291
Sahil Rohira <rohirasahil99@gmail.com>
2017-11-19 15:02:19 +0530

    Committing PackagesInJava

---
+++ src/relational/Compare.java
@@ -0,0 +1,12 @@
+package relational; //name of the package must be mentioned.
+                
+public class Compare {
+   public int getMax(int x, int y) {
+      if ( x > y ) {
+         return x;
+      }
+      else {
+         return y;
+      }
+   }
+}
---
+++ src/relationalDemo/PackageDemo.java
@@ -0,0 +1,11 @@
+package relationalDemo;                    //package is a keyword of Java followed by the package name.
+import relational.Compare;
+public class PackageDemo
+{
+	public static void main(String args[]) {
+	      int val1 = 7, val2 = 9;
+	      Compare comp = new Compare();
+	      int max = comp.getMax(val1, val2); // get the max value
+	      System.out.println("Maximum value is " + max);
+	   }
+	}	
